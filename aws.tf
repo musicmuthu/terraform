bbd486f4daf02b330f7dd0686aafd5a13b4a7f08
muthu <41348854+musicmuthu@users.noreply.github.com>
2022-08-04 10:25:33 +0530

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+        ami = "ami-076e3a557efe1aa9c"
+        instance_type = "t2.micro"
+        count = 1
+        subnet_id = "subnet-0d47c7cc17ec8f644"
+        tags = {
+        Name = "Muthu"
+        }
+}
